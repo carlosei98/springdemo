58d1a36173c5a7f84f74f3b8add4ac6bb33241aa
Carlos Erena <carlos.erena.ibanez@iesjandula.es>
2020-08-22 12:18:37 +0200

    Se ha incluído la librería Spring AOP para añadir un aspecto con la funcionalidad de logger. La clase es: LogAspect Esta clase utiliza a su vez el logger Slf4j para facilitar la tarea de logging. El aspecto intercepta las llamadas al controller y obtiene el método al que se está llamando según la petición HTTP que se realice, muestra un mensaje por pantalla antes de llamar el método, lo llama y luego devuelve el objeto que corresponda.
    Para que todo esto funcione, se ha añadido la etiqueta @EnableAspectJAutoProxy a la clase SpringDemoTrabajadoresApplication

---
+++ src/main/java/com/erena/springdemo/aspects/LogAspect.java
@@ -0,0 +1,33 @@
+package com.erena.springdemo.aspects;
+
+import com.erena.springdemo.model.entities.Trabajador;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LogAspect {
+    @Around("execution(* com.erena.springdemo.controller.TrabajadoresController.*(..))")
+    public Object printLog(@org.jetbrains.annotations.NotNull ProceedingJoinPoint joinPoint) {
+        Object o = null;
+        log.info("Método: " + joinPoint.getSignature().getName());
+        switch (joinPoint.getSignature().getName()) {
+            case "insertar":
+                log.info("Se va a insertar el trabajador: " + joinPoint.getArgs()[0].toString());
+                break;
+            case "getTrabajadores":
+                log.info("Se van a obtener todos los trabajadores");
+                break;
+        }
+        try {
+            o = joinPoint.proceed();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        return o;
+    }
+}
